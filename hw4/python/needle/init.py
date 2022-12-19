746da2a7de783bde1f96deb9da76c1907e67e9f2
PKUFlyingPig <zhongyinmin@pku.edu.cn>
2022-12-19 17:41:05 +0800

    hw4: init funcs

--- hw4/python/needle/init.py
+++ hw4/python/needle/init.py
@@ -70,25 +70,29 @@ def ones_like(array, *, device=None, requires_grad=False):
 
 def xavier_uniform(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    a = gain * math.sqrt(6 / (fan_in + fan_out))
+    return rand(*shape, low=-a, high=a, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2 / (fan_in + fan_out))
+    return randn(*shape, mean=0, std=std, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    bound = math.sqrt(6 / fan_in)
+    return rand(*shape, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_normal(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = math.sqrt(2 / fan_in)
+    return randn(*shape, mean=0, std=std, **kwargs)
     ### END YOUR SOLUTION
